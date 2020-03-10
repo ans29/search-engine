b9a502c93567c54acacb0cf4a0008ea6c89b332e
Anshul <anshul.sharma1@tesco.com>
2020-03-10 18:50:20 +0530

    [Test] file read through streams.

---
+++ src/Constants.java
@@ -0,0 +1,5 @@
+public class Constants
+{
+    public static final String input_file = "Data/wiki-search-small.xml";
+    public static final String output_file = "Data/inverted-index.txt";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,45 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main
 {
+    public static void main(String[] args)
+    {
+      try
+      {
+        readFile (Constants.input_file);
+      } catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+    }
 
+
+    static void readFile (String fileName) throws IOException
+    {
+        Path filePath = Paths.get (fileName);
+
+        try (Stream<String> lines = Files.lines(filePath))
+        {
+
+          List<String> filteredLines = lines
+              .filter(s -> s.contains("gandhi"))
+              .collect(Collectors.toList());
+
+          filteredLines.forEach(System.out::println);
+
+        }
+        catch (IOException e) {
+
+          e.printStackTrace();
+        }
+    }
 }
